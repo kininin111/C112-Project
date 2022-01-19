32df940a41864937cb4de0178eae951045207fd8
kininin111 <87792936+kininin111@users.noreply.github.com>
2022-01-19 18:42:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/thyOqqCJl/model.json',modelLoaded);
+
+  function modelLoaded() {
+      console.log('Model Loaded!');
+  }
+
+  function speak(){
+      var synth = window.speechSynthesis;
+      speak_data_1 = "The first prediction is " + prediction_1;
+      speak_data_2 = "And the second prediction is " + prediction_2;
+      var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+      synth.speak(utterThis);
+  }
+
+  function check()
+  {
+      img = document.getElementById('capture_image');
+      classifier.classify(img, gotResult);
+  }
+
+  function gotResult(error, results) {
+      if (error) {
+          console.error(error);
+      } else {
+          console.log (results);
+          document.getElementById("result_emotion_name").innerHTML = results[0].label;
+          document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+          prediction_1 = results[0].label;
+          prediction_2 = results[1].label;
+          speak();
+          if(result[0].label == "amazing")
+          {
+              document.getElementById("update_emoji").innerHTML = "&#128522;";
+          }
+          if(result[0].label == "victory")
+          {
+              document.getElementById("update_emoji").innerHTML = "&#128532;";
+          }
+          if(result[0].label == "great")
+          {
+              document.getElementById("update_emoji").innerHTML = "&#128548;";
+          }
+
+          if(result[1].label == "amazing")
+          {
+              document.getElementById("update_emoji").innerHTML = "&#128522;";
+          }
+          if(result[1].label == "victory")
+          {
+              document.getElementById("update_emoji").innerHTML = "&#128532;";
+          }
+          if(result[1].label == "great")
+          {
+              document.getElementById("update_emoji").innerHTML = "&#128548;";
+          }
+      }
+  }
